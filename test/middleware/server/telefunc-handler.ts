0174e533198efa55ef5cf282d8ec8956a83c77d7
Romuald Brillout <git@brillout.com>
2024-09-02 23:15:31 +0200

    add test/middleware/

---
+++ test/middleware/server/telefunc-handler.ts
@@ -0,0 +1,21 @@
+import { telefunc } from 'telefunc'
+import type { Get, UniversalHandler } from '@universal-middleware/core'
+
+export const telefuncHandler: Get<[], UniversalHandler> = () => async (request, context, runtime) => {
+  const httpResponse = await telefunc({
+    url: request.url.toString(),
+    method: request.method,
+    body: await request.text(),
+    context: {
+      ...context,
+      ...runtime
+    }
+  })
+  const { body, statusCode, contentType } = httpResponse
+  return new Response(body, {
+    status: statusCode,
+    headers: {
+      'content-type': contentType
+    }
+  })
+}
---
+++ test/middleware/server/vike-handler.ts
@@ -0,0 +1,18 @@
+/// <reference lib="webworker" />
+import { renderPage } from 'vike/server'
+import type { Get, UniversalHandler } from '@universal-middleware/core'
+
+export const vikeHandler: Get<[], UniversalHandler> = () => async (request, context, runtime) => {
+  const pageContextInit = { ...context, ...runtime, urlOriginal: request.url, headersOriginal: request.headers }
+  const pageContext = await renderPage(pageContextInit)
+  const response = pageContext.httpResponse
+  if (!response) throw new Error('Error page missing, make sure to define one https://vike.dev/error-page')
+
+  const { readable, writable } = new TransformStream()
+  response.pipe(writable)
+
+  return new Response(readable, {
+    status: response.statusCode,
+    headers: response.headers
+  })
+}
